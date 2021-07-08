bc74ae23ae281fb7e4a664c1af49ad92ef78f169
LikeDreamwalker <a@ldwid.com>
2021-07-08 20:02:55 +0800

    get started

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-row>
+        <v-col :cols="$vuetify.breakpoint.mdAndUp"> </v-col>
+      </v-row>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    //
+  })
+};
+</script>
