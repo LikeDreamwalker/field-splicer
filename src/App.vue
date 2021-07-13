31ac38842def6feaa5ad31b5eb4c43d8a91d6acb
LikeDreamwalker <a@ldwid.com>
2021-07-13 20:58:12 +0800

    2058

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <v-app>
-    <v-main>
+    <v-main
+      :class="$vuetify.breakpoint.mdAndUp ? 'pa-15' : 'pa-7'"
+      class="d-flex align-center"
+    >
       <v-row>
         <!-- 字段 -->
         <v-col :cols="$vuetify.breakpoint.mdAndUp ? 7 : 12">
@@ -8,74 +11,150 @@
             <!-- 需要让卡片居中 -->
             <v-card
               :elevation="hover ? 24 : 12"
-              class="rounded-xl"
+              class="rounded-xl overflow-y-auto"
               :class="$vuetify.breakpoint.mdAndUp ? 'pa-10' : 'pa-5'"
               height="80vh"
-              :width="$vuetify.breakpoint.mdAndup ? '30vw' : '90vw'"
+              style="overflow-y: visible"
             >
-              <h1 class="mb-5">添加字段</h1>
+              <h1>添加字段</h1>
 
               <v-row
                 v-for="(item, index) in query"
                 :key="item.name"
-                class="d-flex align-baseline"
+                style="margin: 0px"
               >
-                <!-- 按钮组 -->
-                <v-col cols="2">
-                  <v-row class="d-flex align-center">
-                    <!-- 复选框 -->
-                    <v-col cols="4">
-                      <v-checkbox v-model="item.status"></v-checkbox>
-                    </v-col>
-                    <!-- 删除 -->
-                    <v-col cols="4">
-                      <v-btn
-                        icon
-                        color="pink"
-                        @click="this.query.splice(index, 1)"
-                      >
-                        <v-icon>mdi-delete</v-icon>
-                      </v-btn>
-                    </v-col>
-                    <!-- 锁定 -->
-                    <v-col cols="4">
-                      <v-btn
-                        icon
-                        :color="item.onlock ? 'primary' : '#c0c0c0'"
-                        @click="deleteQuery(index)"
-                      >
-                        <v-icon>mdi-lock</v-icon>
-                      </v-btn>
-                    </v-col>
-                  </v-row>
-                </v-col>
-                <!-- 标题 -->
-                <!-- <v-col cols="3">
-                  <v-text-field
-                    clearable
-                    label="名称"
-                    counter
-                    filled
-                    rounded
-                    :disabled="!item.status"
-                    v-model="item.name"
+                <v-hover v-slot:default="{ hover }">
+                  <v-card
+                    :elevation="hover ? 24 : 0"
+                    class="
+                      rounded-xl
+                      d-flex
+                      align-center
+                      justify-end
+                      flex-wrap
+                      mb-3
+                      pa-1
+                    "
+                    width="100%"
                   >
-                  </v-text-field>
-                </v-col> -->
-                <!-- 内容 -->
-                <v-col cols="10">
-                  <v-text-field
-                    :clearable="!item.onlock"
-                    :label="item.name"
-                    counter
-                    filled
-                    rounded
-                    :disabled="!item.status"
-                    :readonly="item.onlock"
-                    v-model="item.content"
+                    <!-- 按钮组 -->
+                    <v-col :cols="$vuetify.breakpoint.mdAndUp ? 2 : 4">
+                      <v-row class="d-flex align-center">
+                        <!-- 复选框 -->
+                        <v-col
+                          :cols="$vuetify.breakpoint.mdAndUp ? 4 : 4"
+                          class="d-flex justify-center"
+                        >
+                          <!-- <v-checkbox v-model="item.status"></v-checkbox> -->
+                          <v-btn
+                            icon
+                            @click="item.status = !item.status"
+                            :color="item.status ? 'primary' : 'secondary'"
+                          >
+                            <v-icon>{{
+                              item.status
+                                ? "mdi-checkbox-marked-circle"
+                                : "mdi-checkbox-blank-circle"
+                            }}</v-icon>
+                          </v-btn>
+                        </v-col>
+                        <!-- 删除 -->
+                        <v-col
+                          :cols="$vuetify.breakpoint.mdAndUp ? 4 : 4"
+                          class="d-flex justify-center"
+                        >
+                          <v-btn
+                            icon
+                            :color="item.onlock ? 'secondary' : 'danger'"
+                            @click="!item.onlock ? deleteQuery(index) : ''"
+                          >
+                            <v-icon>mdi-delete</v-icon>
+                          </v-btn>
+                        </v-col>
+                        <!-- 锁定 -->
+                        <v-col
+                          :cols="$vuetify.breakpoint.mdAndUp ? 4 : 4"
+                          class="d-flex justify-center"
+                        >
+                          <v-btn
+                            icon
+                            :color="
+                              item.status
+                                ? item.onlock
+                                  ? 'primary'
+                                  : 'secondary'
+                                : 'secondary'
+                            "
+                            @click="
+                              item.status ? (item.onlock = !item.onlock) : ''
+                            "
+                          >
+                            <v-icon>mdi-lock</v-icon>
+                          </v-btn>
+                        </v-col>
+                      </v-row>
+                    </v-col>
+                    <!-- 内容 -->
+                    <v-col :cols="$vuetify.breakpoint.mdAndUp ? 10 : 12">
+                      <v-text-field
+                        :clearable="!item.onlock && item.content !== ''"
+                        :label="item.name"
+                        counter
+                        filled
+                        rounded
+                        :disabled="!item.status"
+                        :readonly="item.onlock"
+                        v-model="item.content"
+                        hint="如有必要请输入连接符"
+                      >
+                      </v-text-field>
+                    </v-col>
+                  </v-card>
+                </v-hover>
+              </v-row>
+              <!-- 新建 -->
+              <v-row style="margin: 0px">
+                <v-hover v-slot:default="{ hover }">
+                  <v-card
+                    :elevation="hover ? 24 : 0"
+                    class="
+                      rounded-xl
+                      d-flex
+                      align-center
+                      justify-end
+                      flex-wrap
+                      mb-3
+                      pa-1
+                    "
+                    width="100%"
                   >
-                  </v-text-field>
-                </v-col>
+                    <!-- 新建按钮 -->
+                    <v-col :cols="$vuetify.breakpoint.mdAndUp ? 2 : 4">
+                      <v-row class="d-flex align-center justify-center">
+                        <v-col
+                          :cols="$vuetify.breakpoint.mdAndUp ? 4 : 4"
+                          class="d-flex justify-center"
+                        >
+                          <v-btn icon color="primary" x-large @click="newField">
+                            <v-icon>mdi-plus-circle</v-icon>
+                          </v-btn>
+                        </v-col>
+                      </v-row>
+                    </v-col>
+                    <v-col :cols="$vuetify.breakpoint.mdAndUp ? 10 : 12">
+                      <v-text-field
+                        label="请输入字段名"
+                        counter
+                        filled
+                        rounded
+                        v-model="fieldName"
+                        :clearable="fieldName !== null"
+                        hint="字段名不是字段内容的一部分"
+                      >
+                      </v-text-field>
+                    </v-col>
+                  </v-card>
+                </v-hover>
               </v-row>
             </v-card>
           </v-hover>
@@ -89,15 +168,136 @@
               :class="$vuetify.breakpoint.mdAndUp ? 'pa-10' : 'pa-5'"
               height="80vh"
             >
-              <h1 class="mb-5">拼接结果</h1>
-              <v-textarea
-                :value="fieldSplicer()"
-                filled
-                rounded
-                counter
-                no-resize
-                height="50vh"
-              ></v-textarea>
+              <h1>拼接结果</h1>
+              <v-hover v-slot:default="{ hover }">
+                <v-card
+                  :elevation="hover ? 24 : 0"
+                  class="
+                    rounded-xl
+                    d-flex
+                    align-center
+                    justify-end
+                    flex-wrap
+                    mb-3
+                    pa-4
+                  "
+                  width="100%"
+                  style="overflow-y: visible"
+                >
+                  <v-row>
+                    <!-- 多行文本框 -->
+                    <v-col cols="12">
+                      <v-textarea
+                        :value="fieldSplicer()"
+                        filled
+                        rounded
+                        counter
+                        no-resize
+                        height="20vh"
+                        id="result-textarea"
+                      ></v-textarea>
+                    </v-col>
+                    <!-- 按钮组 -->
+                    <v-col
+                      :cols="$vuetify.breakpoint.mdAndUp ? 6 : 12"
+                      class="d-flex"
+                      :class="
+                        $vuetify.breakpoint.mdAndUp
+                          ? 'justify-start'
+                          : 'justify-center'
+                      "
+                    >
+                      <v-hover v-slot:default="{ hover }">
+                        <v-btn
+                          color="primary"
+                          rounded
+                          :elevation="hover ? 24 : 0"
+                          @click="toNewPage(fieldSplicer())"
+                        >
+                          <v-icon left>mdi-open-in-new</v-icon> 在新标签页中打开
+                        </v-btn>
+                      </v-hover>
+                    </v-col>
+                    <v-col
+                      :cols="$vuetify.breakpoint.mdAndUp ? 6 : 12"
+                      class="d-flex"
+                      :class="
+                        $vuetify.breakpoint.mdAndUp
+                          ? 'justify-end'
+                          : 'justify-center'
+                      "
+                    >
+                      <v-hover v-slot:default="{ hover }">
+                        <v-btn
+                          color="primary"
+                          rounded
+                          :elevation="hover ? 24 : 0"
+                          @click="copyQuery()"
+                        >
+                          <v-icon left>mdi-content-copy</v-icon>
+                          复制字段到剪贴板
+                        </v-btn>
+                      </v-hover>
+                    </v-col>
+                  </v-row>
+                </v-card>
+              </v-hover>
+              <!-- 链接 -->
+              <v-hover v-slot:default="{ hover }">
+                <v-card
+                  :elevation="hover ? 24 : 0"
+                  class="
+                    rounded-xl
+                    d-flex
+                    align-center
+                    justify-center
+                    flex-wrap
+                    mb-3
+                    pa-4
+                  "
+                  width="100%"
+                >
+                  <v-card-title class="justify-center" style="padding: 0px">
+                    访问
+                    <v-btn
+                      color="primary"
+                      text
+                      @click="toNewPage('https://ldwid.com')"
+                    >
+                      ldwid.com
+                    </v-btn>
+                    以获取更多内容
+                  </v-card-title>
+                  <!-- <v-card-text>
+                    <v-btn
+                      icon
+                      color="black"
+                      @click="
+                        toNewPage(
+                          'https://github.com/LikeDreamwalker/field-splicer'
+                        )
+                      "
+                    >
+                      <v-icon>mdi-github</v-icon>
+                    </v-btn>
+                  </v-card-text> -->
+                  <v-row>
+                    <v-col cols="4">
+                      <v-btn
+                        icon
+                        @click="$vuetify.theme.dark = !$vuetify.theme.dark"
+                        :color="$vuetify.theme.dark ? 'danger' : 'primary'"
+                      >
+                        <v-icon>{{
+                          $vuetify.theme.dark
+                            ? "mdi-brightness-4"
+                            : "mdi-brightness-7"
+                        }}</v-icon>
+                      </v-btn>
+                    </v-col>
+                  </v-row>
+                </v-card>
+              </v-hover>
             </v-card>
           </v-hover>
         </v-col>
@@ -119,18 +319,28 @@ export default {
   data: () => ({
     query: [
       {
-        name: "url",
-        content: "localhost:8080",
+        name: "通过添加http/https协议支持跳转",
+        content: "https://",
         status: true,
-        onlock: false
+        onlock: false,
+        clearble: false
       },
       {
-        name: "token",
-        content: "1111111",
+        name: "访问ldwid.com以了解更多",
+        content: "mirrorlake.",
         status: true,
-        onlock: false
+        onlock: false,
+        clearble: false
+      },
+      {
+        name: "LikeDreamwalker",
+        content: "ldwid.com",
+        status: true,
+        onlock: false,
+        clearble: false
       }
-    ]
+    ],
+    fieldName: ""
   }),
   methods: {
     deleteQuery(index) {
@@ -139,9 +349,33 @@ export default {
     fieldSplicer() {
       let result = "";
       for (const item of this.query) {
-        result += item.content;
+        if (item.status && item.content !== null) {
+          result += item.content;
+        }
       }
       return result;
+    },
+    newField() {
+      if (this.fieldName !== "") {
+        let field = {
+          name: "",
+          content: "",
+          status: true,
+          onlock: false,
+          clearble: false
+        };
+        field.name = this.fieldName;
+        this.query.push(field);
+        this.fieldName = "";
+      }
+    },
+    copyQuery() {
+      const copy = document.querySelector("#result-textarea");
+      copy.select();
+      document.execCommand("copy");
+    },
+    toNewPage(location) {
+      window.open(location, "_blank");
     }
   }
 };
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,23 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#0066ff",
+        secondary: "#c0c0c0",
+        danger: "#ff4500",
+        info: "#00bbff",
+      },
+      dark: {
+        primary: "#0066ff",
+        secondary: "#c0c0c0",
+        danger: "#ff4500",
+        info: "#00bbff",
+      },
+    },
+  },
 });
